5e3573bf579bbb516fc360277029b8e648d21071
hgminh523 <hgminh523>
2025-03-25 14:44:55 +0700

    add env

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
